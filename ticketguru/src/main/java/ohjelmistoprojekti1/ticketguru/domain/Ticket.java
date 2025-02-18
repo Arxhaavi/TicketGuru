96069dd97a99d8663b9d43f2a8e9279718d307bc
RikuRoivainen <bhd722@myy.haaga-helia.fi>
2025-02-18 18:48:57 +0200

    Ticket ja TicketType Entityt lisätty

---
+++ ticketguru/src/main/java/ohjelmistoprojekti1/ticketguru/domain/Ticket.java
@@ -0,0 +1,71 @@
+package ohjelmistoprojekti1.ticketguru.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private int ticketId;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_type_id", nullable = false)
+    private TicketType ticketType;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    private boolean ticketUsed;
+
+    public Ticket() {
+
+    }
+
+    public Ticket(TicketType ticketType, Event event, boolean ticketUsed) {
+        this.ticketType = ticketType;
+        this.event = event;
+        this.ticketUsed = ticketUsed;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public TicketType getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(TicketType ticketType) {
+        this.ticketType = ticketType;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public boolean isTicketUsed() {
+        return ticketUsed;
+    }
+
+    public void setTicketUsed(boolean ticketUsed) {
+        this.ticketUsed = ticketUsed;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket [ticketId=" + ticketId + ", ticketType=" + ticketType + ", event=" + event + ", ticketUsed=" + ticketUsed + "]";
+    }
+
+
+
+}
