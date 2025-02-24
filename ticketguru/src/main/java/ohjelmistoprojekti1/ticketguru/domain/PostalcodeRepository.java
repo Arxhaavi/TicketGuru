7ce6ea2dabc605faf59181878410623e94290f5a
Veera <veera.heikkinen95@gmail.com>
2025-02-24 20:14:09 +0200

    Event-, location- ja postalcode-repositoriot

---
+++ ticketguru/src/main/java/ohjelmistoprojekti1/ticketguru/domain/PostalcodeRepository.java
@@ -0,0 +1,7 @@
+package ohjelmistoprojekti1.ticketguru.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostalcodeRepository extends CrudRepository<Postalcode, String> {
+
+}
