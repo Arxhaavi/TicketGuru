1d1ef4440b2844d583dc404d38026d4d4b9a45f3
Arxhaavi <artturihaavisto@hotmail.com>
2025-03-02 18:35:43 +0200

    Get events added

--- ticketguru/src/main/java/web/EventRestController.java
+++ ticketguru/src/main/java/web/EventRestController.java
@@ -1,9 +1,22 @@
 package web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import ohjelmistoprojekti1.ticketguru.domain.Event;
+import ohjelmistoprojekti1.ticketguru.domain.EventRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/events")
 public class EventRestController {
 
+    @Autowired
+    private EventRepository eventRepository;
+
+    @GetMapping()
+    public Iterable<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
 }
