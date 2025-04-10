1558e0e26f272e2ed35e749a05d9776ec15791f9
RikuRoivainen <bhd722@myy.haaga-helia.fi>
2025-04-10 18:48:22 +0300

    Endpoint lipun tarkistusta varten

---
+++ ticketguru/src/main/java/ohjelmistoprojekti1/ticketguru/web/TicketRestController.java
@@ -0,0 +1,52 @@
+package ohjelmistoprojekti1.ticketguru.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ohjelmistoprojekti1.ticketguru.domain.Ticket;
+import ohjelmistoprojekti1.ticketguru.domain.TicketRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketRestController {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @GetMapping("/{id}")
+    public Ticket getTicketById(@PathVariable Long id) {
+        return ticketRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Ticket not found"));
+    }
+
+    @PutMapping("/check/{id}")
+    public Ticket checkTicket(@PathVariable Long id) {
+        Optional<Ticket> optionalTicket = ticketRepository.findById(id);
+        
+        if (optionalTicket.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Ticket not found");
+        }
+
+        Ticket ticket = optionalTicket.get();
+
+        if (ticket.isTicketUsed()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ticket has already been used");
+        }
+
+        ticket.setTicketUsed(true);
+        return ticketRepository.save(ticket);
+        
+    }
+    
+
+}
